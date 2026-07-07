9fe3356d53f6597785c256e34e0a2a9da3a64281
PreetiiKumarii <csaiml23071@glbitm.ac.in>
2026-07-07 13:37:41 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+from openai import OpenAI
+
+# Load .env file
+load_dotenv()
+
+# Gemini Client
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+
+st.title("Preetii AI Chat")
+st.caption("🚀 A chatbot made by Preetii - Ask me anything!")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(
+    model="gemini-2.5-flash" , messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
